703309022918a02a46dd804b71784d0f59eed34e
atcaiola <72409558+atcaiola@users.noreply.github.com>
2020-10-05 13:59:58 -0700

    new commit

--- src/AddingMachine.java
+++ src/AddingMachine.java
@@ -1,24 +1,41 @@
 public class AddingMachine {
   private int total;
+  private StringBuffer memory;
   
+  public static void main(String[] args) {
+    AddingMachine myCalculator = new AddingMachine();
+      
+    myCalculator.add(4); 
+    myCalculator.subtract(2); 
+    myCalculator.add(5);
+    
+    System.out.print(myCalculator.toString());
+  }
+
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    memory = new StringBuffer("0");
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+    total += value;
+    memory.append(" + " + value);
   }
 
   public void subtract (int value) {
+    total -= value;
+    memory.append(" - " + value);
   }
 
   public String toString () {
-    return "";
+    return memory.toString();
   }
 
   public void clear() {
+    memory = new StringBuffer("0");  
   }
 }
